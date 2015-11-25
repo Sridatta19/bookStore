e2f98d23974642ff22b2d17ecd9af7cc6c7bca82
Sridatta Pasumarthy <sridatta.pasumarthy@talentica.com>
2015-11-25 19:30:38 +0530

    no message

--- src/reducers/BookReducer.js
+++ src/reducers/BookReducer.js
@@ -1,6 +1,7 @@
 
 import {List, Map, Record} from 'immutable';
 import {CREATE_BOOK, DELETE_BOOK, EDIT_BOOK} from '../common/ActionTypes';
+import R from 'ramda';
 
 const initialState = [{
     title: 'The Shining',
@@ -9,27 +10,22 @@ const initialState = [{
     id: 0
 }];
 
+const maxId = R.reduce((maxId, book) => Math.max(book.id, maxId), -1);
+const filterBooks = (actionId, book) => book.id != actionId;
+const editBook = (action) => R.merge(R._, { title: action.book.title, author: action.book.author,
+  publisher: action.book.publisher })
+
 export default function books(state = initialState, action) {
   switch(action.type){
     case CREATE_BOOK:
-      return [{
-        id: state.reduce((maxId, book) => Math.max(book.id, maxId), -1) + 1,
-        title: action.book.title,
-        author: action.book.author,
-        publisher: action.book.publisher
-      }, ...state];
+      return R.append({
+        id: maxId(state) + 1, title: action.book.title, author: action.book.author,publisher: action.book.publisher
+      }, state);
     case DELETE_BOOK:
-    return state.filter(book =>
-      book.id !== action.id
-    );
+      debugger;
+      return R.filter(R.curry(filterBooks)(action.id), state);
     case EDIT_BOOK:
-      return state.map(book =>
-        book.id === action.id ?
-          Object.assign({}, book, { title: action.book.title,
-              author: action.book.author,
-              publisher: action.book.publisher }) :
-          book
-      );
+      return R.map(book => book.id === action.id ? R.curry(editBook)(action))(state);
     default:
       return state;
   }
